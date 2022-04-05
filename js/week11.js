71927abcef93e7faea51845bdc1300292e23206f
nima3221 <nima3221@colorado.edu>
2022-04-04 18:39:44 -0600

    bing chilling!

--- js/week11.js
+++ js/week11.js
@@ -1,3 +1,21 @@
-const para = document.querySelector('p')
+const para = document.getElementById('dontClickButton')
 
-para.addEventListener('')
+para.addEventListener('click', updateFail);
+
+function updateFail ()
+{
+   const name = document.getElementById('name');
+   name.textContent = 'YOU';
+
+   const name1 = document.getElementById('name1');
+   name1.textContent = 'SHOULDNT';
+
+   const name2 = document.getElementById('name2');
+   name2.textContent = 'HAVE';
+
+   const name3 = document.getElementById('name3');
+   name3.textContent = 'DONE';
+
+   const name4 = document.getElementById('name4');
+   name4.textContent = 'THAT';
+}
