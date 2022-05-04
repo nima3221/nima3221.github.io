3c24561956a67933cd14e979cc916165c652af2b
nima3221 <nima3221@colorado.edu>
2022-05-04 05:08:14 -0600

    big change!

---
+++ js/final.js
@@ -0,0 +1,7 @@
+var content = document.getElementByTagName('body')[0];
+var darkMode = document.getElementbyId('darkmode');
+darkMode.addEventListener('click', function())
+{
+  darkMode.classList.toggle('active');
+  darkMode.classList.toggle('night');
+}
